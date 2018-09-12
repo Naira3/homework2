a5e30d49c7edd1b66e30c89a314dcaea0ca36d11
Naira Khachatryan <naira_khachatryan@edu.aua.am>
2018-09-12 20:47:34 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const a=13;
+const b=5;
+
+if(a===b) {console.log ("(a+b)/(a-b-1");} 
+else if(b===3 || a===13) {console.log ("(a*b)/(b*2)");}
+else {console.log ("your math is wrong");}
+
+
+const profession= economist;
+const position= financier;
+
+if(profession===economist && position===accountant) {console.log ("Sorry, we don't have info for you");}
+else if(profession===economist || (position===financier && position.lenght!==2)) {console.log ("Hey, you're on right track, we'll help you find the info you search");}
+else {console.log ("Please, follow the link to find info about your profession");}
+
+
+
+ 
